eec2892cbb8e34b81b2a291a2d1a60764a3ae2cb
Daniel Lessenden <dless27@gmail.com>
2020-07-22 14:09:40 -0500

    Create tests for write class

--- test/author_test.rb
+++ test/author_test.rb
@@ -21,8 +21,20 @@ class AuthorTest < MiniTest::Test
     assert_equal [], @charlotte_bronte.books
   end
 
-  def  test_bronte_can_write
+  def  test_jane_class
     jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+  end
+
+  def test_title
+    skip
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
+  def test_bronte_can_write
+    @charlotte_bronte.write("Villette", "1853")
+    assert_equal ["string"], @charlotte_bronte.books
   end
 
 
